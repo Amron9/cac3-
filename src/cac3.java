ca96b1bd26042b72133830443a1a9b21afe4b209
Elysia1989 <Amroncosta123@gmail.com>
2024-01-10 11:14:08 +0530

    working

--- src/cac3.java
+++ src/cac3.java
@@ -1,56 +1,56 @@
-public class cac3 {
+import java.util.Scanner;
 
-    char operator;
-    Double number1, number2, result;
+class Main {
+    public static void main(String[] args) {
 
-    // create an object of Scanner class
-    Scanner input = new Scanner(System.in);
+        char operator;
+        Double number1, number2, result;
 
-    // ask users to enter operator
-    System.out.println("Choose an operator: +, -, *, or /");
-    operator = input.next().charAt(0);
+        // create an object of Scanner class
+        Scanner input = new Scanner(System.in);
 
-    // ask users to enter numbers
-    System.out.println("Enter first number");
-    number1 = input.nextDouble();
+        // ask users to enter operator
+        System.out.println("Choose an operator: +, -, *, or /");
+        operator = input.next().charAt(0);
 
-    System.out.println("Enter second number");
-    number2 = input.nextDouble();
+        // ask users to enter numbers
+        System.out.println("Enter first number");
+        number1 = input.nextDouble();
 
-    switch (operator) {
+        System.out.println("Enter second number");
+        number2 = input.nextDouble();
 
-        // performs addition between numbers
-        case '+':
-            result = number1 + number2;
-            System.out.println(number1 + " + " + number2 + " = " + result);
-            break;
+        switch (operator) {
 
-        // performs subtraction between numbers
-        case '-':
-            result = number1 - number2;
-            System.out.println(number1 + " - " + number2 + " = " + result);
-            break;
+            // performs addition between numbers
+            case '+':
+                result = number1 + number2;
+                System.out.println(number1 + " + " + number2 + " = " + result);
+                break;
 
-        // performs multiplication between numbers
-        case '*':
-            result = number1 * number2;
-            System.out.println(number1 + " * " + number2 + " = " + result);
-            break;
+            // performs subtraction between numbers
+            case '-':
+                result = number1 - number2;
+                System.out.println(number1 + " - " + number2 + " = " + result);
+                break;
 
-        // performs division between numbers
-        case '/':
-            result = number1 / number2;
-            System.out.println(number1 + " / " + number2 + " = " + result);
-            break;
+            // performs multiplication between numbers
+            case '*':
+                result = number1 * number2;
+                System.out.println(number1 + " * " + number2 + " = " + result);
+                break;
 
-        default:
-            System.out.println("Invalid operator!");
-            break;
-    }
+            // performs division between numbers
+            case '/':
+                result = number1 / number2;
+                System.out.println(number1 + " / " + number2 + " = " + result);
+                break;
 
-    input.close();
-}
-}
+            default:
+                System.out.println("Invalid operator!");
+                break;
         }
 
-
+        input.close();
+    }
+}
