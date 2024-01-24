ebdd781de01c8203e3040a6fe5aaf4776995d411
Elysia1989 <Amroncosta123@gmail.com>
2024-01-24 10:07:36 +0530

    new

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class clsjunit {
+
+    public String concate(String one, String two) {
+        return one+two;
+    }
+    public int multiply(int num1, int num2)
+    {
+        return num1*num2;
+    }
+
+
+
+}
